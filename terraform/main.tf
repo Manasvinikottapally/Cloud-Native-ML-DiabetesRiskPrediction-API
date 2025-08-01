5bfecc24e753d47906fdb2a2c204e495cfd20f1b
Manasvini Kottapally <manasvinikottapally@Manasvinis-MacBook-Pro.local>
2025-08-01 19:55:14 -0400

    changed terraform for grafana

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,6 +28,15 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Allow Grafana UI"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
